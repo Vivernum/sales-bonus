03cf52a8146c28cc8453e8ac88d2e2820776c06c
Vivernum_ <pandatv22rus@gmail.com>
2025-08-25 20:11:22 +0700

    fix: exception

--- src/main.js
+++ src/main.js
@@ -107,13 +107,12 @@ function analyzeSalesData(data, options) {
 
     sellerStats.forEach((seller, index) => {
         seller.bonus = calculateBonusByProfit(index, sellerStats.length, seller);
-        seller.top_products = Object.entries(seller.products_sold).map(item => ({
-            sku: item[0],
-            quantity: item[1]
-        })).sort((a, b) => {
+        seller.top_products = Object.entries(seller.products_sold).sort((a, b) => {
         return b.quantity - a.quantity;
         }).slice(0, 10);
     });
+
+    // console.log(sellerStats);
 
     // @TODO: Подготовка итоговой коллекции с нужными полями
 
